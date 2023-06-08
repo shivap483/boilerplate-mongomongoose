ee00c04e901382b112e4d7827f4601130da25554
shiva <shivap483@gmail.com>
2023-06-08 20:47:29 +0530

    added mongo URI

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
-require('dotenv').config();
+const config = require('dotenv');
+config.config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let Person;
 
